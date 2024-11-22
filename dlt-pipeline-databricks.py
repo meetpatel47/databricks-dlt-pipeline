1f31cca5713837ad250ddf1d81960f1ac19682d9
imeetpatel47@gmail.com <imeetpatel47@gmail.com>
2024-11-22 03:42:59 +0000

    Ok

--- dlt-pipeline-databricks.py
+++ dlt-pipeline-databricks.py
@@ -1,68 +1,54 @@
 # Databricks notebook source
+# DBTITLE 1,Bronze Layer
 # MAGIC %sql
-# MAGIC CREATE STREAMING LIVE TABLE customers
-# MAGIC COMMENT "The customers buying finished products, ingested from /databricks-datasets."
-# MAGIC TBLPROPERTIES ("myCompanyPipeline.quality" = "mapping")
-# MAGIC AS SELECT * FROM cloud_files("/databricks-datasets/retail-org/customers/", "csv");
+# MAGIC CREATE OR REFRESH STREAMING LIVE TABLE sales_raw
+# MAGIC COMMENT "The raw sales data,ingested from sales-raw"
+# MAGIC AS SELECT * FROM cloud_files("${datasets.path}/sales-raw", "csv")
 
 # COMMAND ----------
 
+# DBTITLE 1,Bronze Layer
 # MAGIC %sql
-# MAGIC CREATE STREAMING LIVE TABLE sales_orders_raw
-# MAGIC COMMENT "The raw sales orders, ingested from /databricks-datasets."
-# MAGIC TBLPROPERTIES ("myCompanyPipeline.quality" = "bronze")
-# MAGIC AS
-# MAGIC SELECT * FROM cloud_files("/databricks-datasets/retail-org/sales_orders/", "json", map("cloudFiles.inferColumnTypes", "true"))
+# MAGIC CREATE OR REFRESH LIVE TABLE customers
+# MAGIC COMMENT "The customers lookup table, ingested from customers-csv"
+# MAGIC AS SELECT * FROM csv.`${datasets.path}/customers-csv`
 
 # COMMAND ----------
 
+# DBTITLE 1,Silver Layer
 # MAGIC %sql
-# MAGIC CREATE STREAMING LIVE TABLE sales_orders_cleaned(
-# MAGIC   CONSTRAINT valid_order_number EXPECT (order_number IS NOT NULL) ON VIOLATION DROP ROW
+# MAGIC CREATE OR REFRESH STREAMING LIVE TABLE sales_cleaned (
+# MAGIC   CONSTRAINT valid_sales_number EXPECT (sales_id IS NOT NULL) ON VIOLATION DROP ROW
 # MAGIC )
-# MAGIC PARTITIONED BY (order_date)
-# MAGIC COMMENT "The cleaned sales orders with valid order_number(s) and partitioned by order_datetime."
-# MAGIC TBLPROPERTIES ("myCompanyPipeline.quality" = "silver")
+# MAGIC COMMENT "The cleaned sales orders with valid sales_id"
 # MAGIC AS
-# MAGIC SELECT f.customer_id, f.customer_name, f.number_of_line_items, 
-# MAGIC   TIMESTAMP(from_unixtime((cast(f.order_datetime as long)))) as order_datetime, 
-# MAGIC   DATE(from_unixtime((cast(f.order_datetime as long)))) as order_date, 
-# MAGIC   f.order_number, f.ordered_products, c.state, c.city, c.lon, c.lat, c.units_purchased, c.loyalty_segment
-# MAGIC   FROM STREAM(LIVE.sales_orders_raw) f
+# MAGIC   SELECT c.sale_id, c.quantity, c.customer_id,c.first_name as f_name, c.last_name as l_name,s.products,
+# MAGIC          c.country as country
+# MAGIC   FROM STREAM(LIVE.sales_raw) s
 # MAGIC   LEFT JOIN LIVE.customers c
-# MAGIC       ON c.customer_id = f.customer_id
-# MAGIC      AND c.customer_name = f.customer_name
+# MAGIC     ON s.customer_id = c.customer_id
 
 # COMMAND ----------
 
+# DBTITLE 1,Gold Layer
 # MAGIC %sql
-# MAGIC CREATE LIVE TABLE sales_order_in_la
-# MAGIC COMMENT "Sales orders in LA."
-# MAGIC TBLPROPERTIES ("myCompanyPipeline.quality" = "gold")
+# MAGIC
+# MAGIC CREATE OR REFRESH LIVE TABLE sales_products
+# MAGIC COMMENT "Average products per customer in Singapore"
 # MAGIC AS
-# MAGIC SELECT city, order_date, customer_id, customer_name, ordered_products_explode.curr, SUM(ordered_products_explode.price) as sales, SUM(ordered_products_explode.qty) as quantity, COUNT(ordered_products_explode.id) as product_count
-# MAGIC FROM (
-# MAGIC   SELECT city, order_date, customer_id, customer_name, EXPLODE(ordered_products) as ordered_products_explode
-# MAGIC   FROM LIVE.sales_orders_cleaned 
-# MAGIC   WHERE city = 'Los Angeles'
-# MAGIC   )
-# MAGIC GROUP BY order_date, city, customer_id, customer_name, ordered_products_explode.curr
+# MAGIC   SELECT customer_id,f_name,l_name,avg(products) avg_products
+# MAGIC   FROM LIVE.sales_cleaned
+# MAGIC   WHERE country = "singapore"
+# MAGIC   GROUP BY customer_id
 
 # COMMAND ----------
 
+# DBTITLE 1,Gold Layer
 # MAGIC %sql
-# MAGIC CREATE LIVE TABLE sales_order_in_chicago
-# MAGIC COMMENT "Sales orders in Chicago."
-# MAGIC TBLPROPERTIES ("myCompanyPipeline.quality" = "gold")
+# MAGIC CREATE OR REFRESH LIVE TABLE sales_product
+# MAGIC COMMENT "Number of products per customer country wise"
 # MAGIC AS
-# MAGIC SELECT city, order_date, customer_id, customer_name, ordered_products_explode.curr, SUM(ordered_products_explode.price) as sales, SUM(ordered_products_explode.qty) as quantity, COUNT(ordered_products_explode.id) as product_count
-# MAGIC FROM (
-# MAGIC   SELECT city, order_date, customer_id, customer_name, EXPLODE(ordered_products) as ordered_products_explode
-# MAGIC   FROM LIVE.sales_orders_cleaned 
-# MAGIC   WHERE city = 'Chicago'
-# MAGIC   )
-# MAGIC GROUP BY order_date, city, customer_id, customer_name, ordered_products_explode.curr
-
-# COMMAND ----------
-
-
+# MAGIC   SELECT customer_id, f_name, l_name,dense_rank()over(partition by country order by products desc) number_of_products
+# MAGIC   FROM LIVE.sales_cleaned
+# MAGIC   GROUP BY customer_id
+# MAGIC
